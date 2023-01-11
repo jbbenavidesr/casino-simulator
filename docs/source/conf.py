9b57b85b98779ddf79e1ec6a0f7d0a97d82f9f16
Juan Bernardo Benavides Rubio <juan.benavides@fintegra.co>
2023-01-11 14:15:05 -0500

    :memo: Add extensions to documentation generation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Casino Simulator'
-copyright = '2023, Juan Bernardo Benavides Rubio'
-author = 'Juan Bernardo Benavides Rubio'
+project = "Casino Simulator"
+copyright = "2023, Juan Bernardo Benavides Rubio"
+author = "Juan Bernardo Benavides Rubio"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
